8f1ebeefb0f197405c126034be3e9c6826faf9a4
FloofyBunny <artem.didytschuk@gmx.de>
2023-04-21 20:37:42 +0200

    Fixed to top large nav & Techstack added

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 "use strict"
-
-
-function showDropDown(id) {
-    produkte.classList.toggle('show')
-}
-
-produkte = document.getElementById('produkte')
-produkte.addEventListener('mouseover', showDropDown(produkte));

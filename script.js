27057b2457e84942d5d48895b0f701a99c915591
FloofyBunny <artem.didytschuk@gmx.de>
2023-04-22 16:50:33 +0200

    Technologie Bezeichnungen

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 //cycle techstack elements animations
 let id = 1;
 function currentAnimation() {
-
-    let item = document.querySelector("#techstack img:nth-child(" + id + ")" );
+    
+    let item = document.querySelector("#techstack div:nth-child(" + id + ") img" );
     item.classList.remove("animation");
 
     id = Math.floor(Math.random() * 7) + 1;
-    item = document.querySelector("#techstack img:nth-child(" + id + ")" );
+    item = document.querySelector("#techstack div:nth-child(" + id + ") img" );
     item.classList.add("animation");
+
 }
 
 setInterval(currentAnimation, 3000);

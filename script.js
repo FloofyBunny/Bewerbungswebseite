eac2e35e7ea020527a27b2d4fae06173f5b6c409
FloofyBunny <artem.didytschuk@gmx.de>
2023-04-22 14:27:12 +0200

    Techstack image animations added

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict"
+
+// reset Animation when user refocuses
+window.addEventListener("focus", () => {
+    for (let i = 1; i <= 7; i++){
+        let item = document.querySelector("#techstack img:nth-child(" + i + ")" );
+        item.style.transform = "rotate(0deg)";
+        item.style.transform = "scale(1)";
+    }
+});
+
+//cycle techstack elements animations
+let id = 1;
+function currentAnimation() {
+
+    let item = document.querySelector("#techstack img:nth-child(" + id + ")" );
+    item.style.animationPlayState = "paused";
+
+    id = Math.floor(Math.random() * 7) + 1;
+    item = document.querySelector("#techstack img:nth-child(" + id + ")" );
+    item.style.animationPlayState = "running";
+}
+
+setInterval(currentAnimation, 3000);

5ee1b3f5727c98717a66c31b04d01390ad301d8f
FloofyBunny <artem.didytschuk@gmx.de>
2023-04-22 22:44:15 +0200

    carousel caption responsive

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 "use strict"
 
+
+
+function carouselCaption() {
+
+    if(window.innerWidth < 1200){
+        let prev = document.querySelector("#carousel .show");
+
+        for(let i = 1; i<= 3; i++){
+            let x = document.getElementById("car-" + i);
+            if(x.classList.contains("active")) {
+                let y = document.querySelector(".small-caption:nth-child("+ i + ")");
+
+                if(y !== prev && prev !== null){
+                    prev.classList.remove("show");
+                    prev.classList.add("d-none");
+                }
+                y.classList.remove("d-none");
+                y.classList.add("show");
+            }
+        }
+    } else {
+        let x = document.querySelectorAll(".small-caption");
+        x.forEach((e) => {e.classList.add("d-none");});
+        x.forEach((e) => {e.classList.remove("show");});
+        
+    }
+}
+setInterval(carouselCaption, 100);
+
+
+
+
+
+
+
+
 //cycle techstack elements animations
 let id = 1;
 function currentAnimation() {
@@ -12,5 +48,6 @@ function currentAnimation() {
     item.classList.add("animation");
 
 }
+
 
 setInterval(currentAnimation, 3000);
